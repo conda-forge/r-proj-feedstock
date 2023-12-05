9aa9b964743da293885e071f4476e87e72fa7dce
Mervin Fansler <fanslerm@mskcc.org>
2023-12-05 18:16:33 -0500

    set configuration by script

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,6 @@
-REM include POSIX'd paths
-set "PKG_CPPFLAGS=-I%LIBRARY_INC:\=/%"
-set "PKG_LIBS=-L%LIBRARY_LIB:\=/%
+REM add configuration to source
+cp %RECIPE_DIR%/configure.win .
+IF %ERRORLEVEL% NEQ 0 exit /B 1
 
 "%R%" CMD INSTALL --build . %R_ARGS%
 IF %ERRORLEVEL% NEQ 0 exit /B 1
